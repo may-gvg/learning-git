4012e87f6f3a024a798359ea5edd2b9499e6a063
may <83417030+may-gvg@users.noreply.github.com>
2021-04-29 18:51:01 +0200

    zmiana 1

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print ("Hiszpańska inkwizycja' to najlepszy skecz grupy Monthy Pythona")
+print ("")
+
+lista_zakupów = {
+    "Warzywniak": ['Marchew', 'Seler', 'Rukola'],
+    "Piekarnia": ['Chleb', 'Pączek', 'Bułki']
+}
+
+print ("Lista Zakupów")
+suma = 0
+for sklep, produkty in lista_zakupów.items():
+    print(f"Idę do {sklep.upper()}")
+    print("kupuję tu następujące rzeczy")
+    for produkt in produkty:
+        print(produkt.upper())
+        suma = suma + 1
+
+print("Suma produktów: ", suma)
